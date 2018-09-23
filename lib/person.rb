15c53e8e9929e37237f8078936317a85f9f35c90
zhifeng ou <zhifengou@hotmail.com>
2018-09-23 22:43:39 +0000

    Done.

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,9 @@
 class Person
-  #your code here
+  attr_accessor:name, :birthday, :hair_color, :eye_color, :height,:weight, :handed, :complexion, :t_shirt_size, :wrist_size, :glove_size, :pant_length, :pant_width
+
+def initialize(attr)
+  attr.each do |key,val|
+    self.send("#{key}=",val)
+end
+end
 end
